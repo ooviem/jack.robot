3649cbcd2c50271ba8185d3d96e041ebb74d9bc3
Viem Ong <viem.oo22992@gmail.com>
2016-01-22 23:48:58 +0700

    no message

--- robot/robot.js
+++ robot/robot.js
@@ -6,10 +6,19 @@ Cylon.robot({
   },
 
   devices: {
-    led: { driver: 'led', pin: 3 }
+    led: { driver: 'led', pin: 7 },
+    led1: { driver: 'led', pin: 11 },
+    led2: { driver: 'led', pin: 13 },
+    led3: { driver: 'led', pin: 15 },
+
+
   },
 
   work: function(my) {
     every((1).second(), my.led.toggle);
+    every((1).second(), my.led1.toggle);
+    every((1).second(), my.led2.toggle);
+    every((1).second(), my.led3.toggle);
+
   }
 }).start();
