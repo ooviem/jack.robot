57b4e19cdb516da35a1ad367e673c127dc5fe7b3
Viem Ong <viem.oo22992@gmail.com>
2016-02-04 12:32:10 +0700

    no message

--- robot/robot.js
+++ robot/robot.js
@@ -1,26 +1,21 @@
-"use strict"
+Cylon.robot({
+  connections: {
+    arduino: { adaptor: 'raspi' }
+  },
 
-var Cylon = require("cylon");
+  devices: {
+    pin11: { driver: 'direct-pin', pin: 11 },
+    pin13: { driver: 'direct-pin', pin: 13 },
+    pin19: { driver: 'direct-pin', pin: 19 },
+    pin21: { driver: 'direct-pin', pin: 21 }
 
-Cylon
-  .robot()
-  .connection("raspi", { adaptor: "raspi", port: "/dev/ttyACM0" })
-  .device("servo", {
-    driver: "servo",
-    pin: 11,
-    limits: { bottom: 20, top: 160 }
-  })
-  .on("ready", function(bot) {
-    var angle = 30,
-    increment = 40;
+  },
 
-    setInterval(function() {
-      angle += increment;
-      bot.servo.angle(angle);
-      console.log("Current Angle: " + (bot.servo.currentAngle()));
+  work: function(my) {
+    my.pin11.digitalWrite(0);
+    my.pin13.digitalWrite(1);
+    my.pin19.digitalWrite(0);
+    my.pin21.digitalWrite(1);
 
-      if ((angle === 30) || (angle === 150)) { increment = -increment; }
-    }, 1000);
-  });
-
-Cylon.start();
+  }
+}).start();
