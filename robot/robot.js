30446081c24e52efb4d6ee464ad38fbe742872e9
Viem Ong <viem.oo22992@gmail.com>
2016-02-04 14:35:50 +0700

    no message

--- robot/robot.js
+++ robot/robot.js
@@ -1,6 +1,10 @@
 var Cylon = require('cylon');
 
-Cylon.api('http');
+Cylon.api('http', {
+  ssl: false // serve unsecured, over HTTP
+  // optional configuration here.
+  // for details see 'Configuration' section.
+});
 
 Cylon.robot({
   connections: {
