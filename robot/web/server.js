dfcb60603d5b027fb55f78c943b0cfb2fc2bf8a0
Viem Ong <viem.oo22992@gmail.com>
2016-07-24 18:46:48 +0700

    change server init paht to fs

--- robot/web/server.js
+++ robot/web/server.js
@@ -9,33 +9,34 @@ module.exports = {
             var uri = url.parse(request.url).pathname,
                 filename = path.join(process.cwd()+"/web/", uri);
 
-            path.exists(filename, function(exists) {
-                if (!exists) {
-                    response.writeHead(404, {
+            var exists = fs.exists(filename);
+
+            if (!exists) {
+                response.writeHead(404, {
+                    "Content-Type": "text/plain"
+                });
+                response.write("404 Not Found\n");
+                response.end();
+                return;
+            }
+
+            if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+            fs.readFile(filename, "binary", function(err, file) {
+                if (err) {
+                    response.writeHead(500, {
                         "Content-Type": "text/plain"
                     });
-                    response.write("404 Not Found\n");
+                    response.write(err + "\n");
                     response.end();
                     return;
                 }
 
-                if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-                fs.readFile(filename, "binary", function(err, file) {
-                    if (err) {
-                        response.writeHead(500, {
-                            "Content-Type": "text/plain"
-                        });
-                        response.write(err + "\n");
-                        response.end();
-                        return;
-                    }
-
-                    response.writeHead(200);
-                    response.write(file, "binary");
-                    response.end();
-                });
+                response.writeHead(200);
+                response.write(file, "binary");
+                response.end();
             });
+
         }).listen(parseInt(port, 10));
 
         console.log("Controller running at\n  => http://localhost/");
