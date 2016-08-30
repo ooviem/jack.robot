8f1c82ac8071db52001acbeb1f97562957eee60b
Viem Ong <viem.oo22992@gmail.com>
2016-08-30 23:03:38 +0700

    no message

--- robot/utils/command.js
+++ robot/utils/command.js
@@ -5,16 +5,13 @@ var Q = require("q");
 var Command = {
    
     execute: function (command) {
-        var deferred = q.defer();
         console.log(fullCommand);
         cp.exec(fullCommand, function (error, stdout, stderr) {
             var output = {
                 error: error,
                 stderr: stderr
             };
-            deferred.resolve(output);
         });
-        return deferred.promise;
     },
     speak: function(text){
         var command = "flite -voice RMS '"+ text +"'";
