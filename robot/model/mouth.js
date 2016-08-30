62feca8c58a281ac8070765e4984c8e9a637dc36
Viem Ong <viem.oo22992@gmail.com>
2016-08-30 22:54:07 +0700

    no message

--- robot/model/mouth.js
+++ robot/model/mouth.js
@@ -1,7 +1,7 @@
 var command = require("../utils/command.js");
-module.exports = function() {
-	this.speak = function(text){
+module.exports = {
+	speak: function(text){
 		console.log("SPEAK: "+ text);
 		command.speak(text);
-	};
+	}
 };
