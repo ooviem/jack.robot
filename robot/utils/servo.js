604bba6beb1fe1da57b83456f9e03ab287a6f8ad
Viem Ong <viem.oo22992@gmail.com>
2016-09-01 23:08:51 +0700

    no message

--- robot/utils/servo.js
+++ robot/utils/servo.js
@@ -1,5 +1,5 @@
 var makePwm = require('../adafruit-pca9685/adafruit-pca9685.js');
-var pwm = makePwm({"freq": 60, "correctionFactor": 1.118});
+var pwm = makePwm({"freq": 50, "correctionFactor": 1.118});
 var setServoPulse = function(channel, pulse) {
   var pulseLength;
   pulseLength = 1000000;
@@ -7,7 +7,12 @@ var setServoPulse = function(channel, pulse) {
   pulseLength /= 4096;
   pulse *= 1000;
   pulse /= pulseLength;
+  console.log(pulse);
   return pwm.setPwm(channel, 0, pulse);
 };
-setServoPulse(0, 200);
-setServoPulse(0, 400);
+setServoPulse(0, 555);
+setServoPulse(0, 777);
+setServoPulse(1, 555);
+setServoPulse(1, 777);
+setServoPulse(1, 555);
+setServoPulse(0, 555);
