951287a94ce5b8606614c091e16cf170cdf9e998
Viem Ong <viemong@pingidentity.com>
2016-09-10 08:02:08 +0700

    no message

--- robot/model/body.js
+++ robot/model/body.js
@@ -44,11 +44,11 @@ module.exports = function(hardwareIO) {
                                     foot.stop();
                                     head.turn(560);
                                     foot.turnLeft();
-                                    after((0.8).seconds(), function(){
+                                    after((0.6).seconds(), function(){
                                         foot.stop();      
                                         head.turn(410);
                                         foot.runForward();
-                                        after((1.2).seconds(), function(){
+                                        after((1).seconds(), function(){
                                             foot.stop();
                                             head.turn(250);
                                             foot.turnRight();
